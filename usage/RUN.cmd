ea7c2cfde738342093cd293bbba2700dd50971b7
KHAAdotPK <spysee.pk@gmail.com>
2024-11-10 00:25:37 +0500

    Now chat bot reads from binary data files, these files have the trained weights.

--- usage/RUN.cmd
+++ usage/RUN.cmd
@@ -1,1 +1,2 @@
-.\weights.exe words sunshine time soni kisses sunshine mamal bowhead whale
+REM .\weights.exe words sunshine time soni kisses sunshine mamal bowhead whale
+.\weights.exe sunshine whale whale kisses cheeks w1 w2p.dat
